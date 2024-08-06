7e61b45690ab904db053000a4e8a660b1ad9de33
siberianshark <siberianshark@github.com>
2024-08-06 22:17:19 +0700

    quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import React from 'react';
 import './index.scss';
 
-function App() {
-  const[open, setOpen] = React.useState(false);
-  return(
-    <div className="App">
-      <button onClick={() => setOpen(true)} className="open-modal-btn">
-        ✨ Открыть окно
-        </button>
-      
-      <Modal open={open} setOpen={setOpen} >
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-          <h3>
-          modal window
-          <button>12345546</button>
-          </h3>
-      </Modal>
-      
-      
-    </div>)}
-  ;
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
 
+function Result({correct}) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
+      <a href='/'>
+      <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({step, question, onClickVariant}) {
+  const percentage = Math.round((step / questions.length) * 100);
+  console.log(percentage);
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+
+        {
+          question.variants.map((text, index)=>(
+             <li onClick={()=> onClickVariant(index)} key={text}>{text}</li>
+            ))}
+      </ul>
+    </>
+  );
+}
+function App() {
+  const [step, setStep]= React.useState(0);
+  const [correct, setCorrect]= React.useState(0);
+  const question = questions[step];
+  const onClickVariant = (index) =>{
+  console.log(step,index);
+  setStep(step + 1);
+  if (index=== question.correct){
+    setCorrect(correct +1);
+  }
+  }
+  //console.log(question);
+  return (
+    <div className="App">{
+      step !== question.length?(
+      <Game 
+      
+      step={step} question={question}  onClickVariant={onClickVariant}/> ): (
+      <Result correct={correct} /> )
+}
+      {/* */}
+    </div>
+  );
+}
 
 export default App;
